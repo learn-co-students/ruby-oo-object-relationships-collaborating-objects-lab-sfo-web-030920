59a83714de6e4f93a63ea265dc19d503dada067f
caicindy87 <caicindy87@gmail.com>
2020-03-15 10:07:08 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,9 @@ class Song
 
     def artist_name=(artist_str)
         artist_ins = Artist.find_or_create_by_name(artist_str)
-        Artist.add_song(self)
+        # self.artist = artist_ins  # a method (.add_song) in Artist class already defined to do this function, just need to call it
+        artist_ins.add_song(self) # this way better than the line above because it collaborates with Artist class
+
     end
 
 end
