6e24efe50fb067764a4a079f3932b3375dacc321
SaberChulainn <Tbandachit@gmail.com>
2020-03-17 09:43:51 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,8 @@ class Song
     end
 
     def self.new_by_filename(file)
-        song = file.split(" - ")
-        newSong = Song.new(song[1])
+        song = file.split(" - ") #Becomes an array with artist at 1st element, artist at 0 element
+        newSong = Song.new(song[1]) 
         artist = Artist.new(song[0])
         newSong.artist = artist
         return newSong
@@ -23,7 +23,7 @@ class Song
     end
 
     def artist_name=(artist_name)
-        artist = Artist.find_or_create_by_name(artist_name)
+        artist = Artist.find_or_create_by_name(artist_name) 
         self.artist = artist
     end
 
